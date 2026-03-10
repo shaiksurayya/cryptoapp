f545a967c2ef1cef3903b3fbd7f9ac46bb14f369
Shaik Surayya <shaiksurayyatabassum@gmail.com>
2026-03-10 12:07:04 +0530

    Fix import path for useGetCryptosQuery

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
-import { useGetCryptosQuery } from '../services/CryptoApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
