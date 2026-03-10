15afd61332cc9145dcd81cdb402e7622a1cbe09e
Shaik Surayya <shaiksurayyatabassum@gmail.com>
2026-03-10 10:01:05 +0530

    Update GNEWS_API_KEY to a hardcoded value
    Replaced the GNEWS_API_KEY with a hardcoded value.

--- src/services/cryptoNewsApi.jsx
+++ src/services/cryptoNewsApi.jsx
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const GNEWS_API_KEY = process.env.REACT_APP_GNEWS_API_KEY;
+const GNEWS_API_KEY = '44e79c884c8f3fe6dacc510b5c7dc15b';
 
 const baseUrl = 'https://gnews.io/api/v4';
 
